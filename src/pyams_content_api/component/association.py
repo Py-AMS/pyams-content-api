7c1b4a4f7edb41bd89fc416c37a78add86ea2093
Thierry Florac <thierry.florac@onf.fr>
2025-01-01 20:09:18 +0100

    Updated associations exporters

--- src/pyams_content_api/component/association.py
+++ src/pyams_content_api/component/association.py
@@ -8,20 +8,43 @@
 # FOR A PARTICULAR PURPOSE.
 #
 
-__docformat__ = 'restructuredtext'
-
 from pyramid.interfaces import IRequest
 
 from pyams_content.component.association import IAssociationContainer, IAssociationContainerTarget
 from pyams_content.component.association.interfaces import IAssociationParagraph
 from pyams_content_api.component.paragraph import JSONBaseParagraphExporter
-from pyams_content_api.feature.json import IJSONExporter
+from pyams_content_api.feature.json import IJSONExporter, JSONBaseExporter
 from pyams_utils.adapter import adapter_config
 
+__docformat__ = 'restructuredtext'
+
+
+def get_associations(context, request, **params):
+    """Associations getter"""
+    associations = []
+    registry = request.registry
+    container = IAssociationContainer(context)
+    for association in container.get_visible_items(request):
+        exporter = registry.queryMultiAdapter((association, request), IJSONExporter)
+        if exporter is not None:
+            output = exporter.to_json(**params)
+            if output:
+                associations.append(output)
+    return associations
 
 @adapter_config(name='associations',
                 required=(IAssociationContainerTarget, IRequest),
                 provides=IJSONExporter)
+class JSONAssociationContainerTargetExporter(JSONBaseExporter):
+    """JSON association container target exporter"""
+    
+    is_inner = True
+    conversion_target = None
+    
+    def convert_content(self, **params):
+        return get_associations(self.context, self.request, **params)
+
+    
 @adapter_config(required=(IAssociationParagraph, IRequest),
                 provides=IJSONExporter)
 class JSONAssociationParagraphExporter(JSONBaseParagraphExporter):
@@ -29,15 +52,7 @@ class JSONAssociationParagraphExporter(JSONBaseParagraphExporter):
     
     def convert_content(self, **params):
         result = super().convert_content(**params)
-        associations = []
-        registry = self.request.registry
-        container = IAssociationContainer(self.context)
-        for association in container.get_visible_items(self.request):
-            exporter = registry.queryMultiAdapter((association, self.request), IJSONExporter)
-            if exporter is not None:
-                output = exporter.to_json(**params)
-                if output:
-                    associations.append(output)
+        associations = get_associations(self.context, self.request, **params)
         if associations:
             result['associations'] = associations
         return result
